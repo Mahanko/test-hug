3ee945f5d17bdb071b5fca997d8f772c13079c2a
Mahanko <b9197891075@gmail.com>
2024-06-28 22:00:23 +0330

    newnewnew

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ let dark = document.getElementById('dark');
 dark.addEventListener('click',()=>{
     document.body.classList.toggle('dark');
 })
+
+let menubutoon = document.getElementById('menubutoon');
+let menuslide = document.getElementById('menuslide');
+let closemenu = document.getElementById('close');
+menubutoon.addEventListener('click',()=>{
+    menuslide.setAttribute("style","display: block");
+})
+closemenu.addEventListener('click',()=>{
+    menuslide.setAttribute("style","display: none");
+})
